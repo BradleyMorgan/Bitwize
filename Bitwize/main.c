1bdeea01bad30cae676210fcf802c6038bde76eb
Bradley Morgan <morganconsulting@hotmail.com>
2012-07-09 15:22:50 -0500

    Initial Commit

---
+++ Bitwize/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Bitwize
+//
+//  Created by Bradley Morgan on 7/9/12.
+//  Copyright (c) 2012 Morgan Consulting. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
